2eaa0ac206b8efcfc52b94e51c71664c01742687
swrnv <64857573+swrnv@users.noreply.github.com>
2021-07-13 15:19:44 +0530

    Add files via upload

---
+++ src/app_component/weather.component.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+
+
+const Weather = (props) =>{
+    return(
+        <div className="container text-light">
+            <div className="cards pt-4">
+            <h1>Check My Weather</h1>
+            <h1>{props.city}</h1>
+            <h5 className="py-4">
+                <i className={`wi ${props.weatherIcon} display-1`}></i>
+            </h5>
+           {props.temp_celcius ? ( <h1 className="py-2">
+            {props.temp_celcius}&deg;
+            </h1>):null}
+            {minmaxtemp(props.temp_min,props.temp_max)}
+            {humidity_windspeed(props.humidity, props.wind_speed)}
+            <h4 className="py-3">{props.description}</h4>
+            </div>
+        </div>
+    );
+};
+
+
+function minmaxtemp(min, max) {
+    if(min&&max){
+    return(
+        <h3>
+            <span className="px-4">Min - {min}&deg;</span>
+            <span className="px-4">Max - {max}&deg;</span>
+        </h3>
+    );}
+}
+
+function humidity_windspeed(humidity, wind_speed) {
+    if(humidity&&wind_speed){
+    return(
+        <h3>
+            <span className="px-4">Humidity - {humidity}</span>
+            <span className="px-4">Wind Speed - {wind_speed}</span>
+        </h3>
+    );}
+}
+
+export default Weather;
